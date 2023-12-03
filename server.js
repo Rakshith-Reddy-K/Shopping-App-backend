e9d8862e5b78a01657e1e00b94193d6a90067bca
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-12-03 18:56:06 -0500

    Updated package.json and server.js files by adding PostgreSQL database setup, dotenv configuration, and CRUD operations for products, users, cart, comments, and follows tables. (generated by blackbox.ai )

--- server.js
+++ server.js
@@ -1,177 +1,65 @@
-// server.js
+require('dotenv').config();
+
 const express = require('express');
-const axios = require('axios');
-const sqlite3 = require('sqlite3').verbose();
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
-// Initialize the SQLite database
-const db = new sqlite3.Database(':memory:');
+// PostgreSQL connection setup
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    
+});
+const initializeTables = async () => {
+    try {
+        await pool.query("CREATE TABLE IF NOT EXISTS products (id SERIAL PRIMARY KEY, title TEXT, price REAL, description TEXT, category TEXT, image TEXT, rate REAL, count INT)");
+        await pool.query("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, is_active BOOLEAN)");
+        await pool.query("CREATE TABLE IF NOT EXISTS cart (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), product_id INTEGER REFERENCES products(id))");
+        await pool.query("CREATE TABLE IF NOT EXISTS comments (id SERIAL PRIMARY KEY, comment TEXT, likes INTEGER, product_id INTEGER REFERENCES products(id))");
+        await pool.query("CREATE TABLE IF NOT EXISTS follows (id SERIAL PRIMARY KEY, seller_id INTEGER REFERENCES users(id), user_id INTEGER REFERENCES users(id))");
+    } catch (error) {
+        console.error('Error initializing database tables:', error);
+    }
+};
 
-db.serialize(() => {
-    db.run("CREATE TABLE products (id INT, title TEXT, price REAL, description TEXT, category TEXT, image TEXT, rate REAL, count INT)");
-
-    axios.get('https://fakestoreapi.com/products')
-        .then(response => {
-            const stmt = db.prepare("INSERT INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-            response.data.forEach(product => {
-                stmt.run(product.id, product.title, product.price, product.description, product.category, product.image, product.rating.rate, product.rating.count);
-            });
-            stmt.finalize();
-        });
-       // db.run("CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY,comment TEXT,like INTEGER productId INTEGER,FOREIGN KEY (productId) REFERENCES products(id)")
-       // db.run("CREATE TABLE IF NOT EXISTS follows (id INTEGER PRIMARY KEY,sellerId INTEGER,userId INTEGER,FOREIGN KEY (sellerId) REFERENCES user(Id),FOREIGN KEY (userId) REFERENCES user(Id)")
-        db.run("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, username TEXT, password TEXT, isActive BOOLEAN)");
-        db.run("CREATE TABLE IF NOT EXISTS cart (id INTEGER PRIMARY KEY, userId INTEGER, productId INTEGER, FOREIGN KEY (userId) REFERENCES user(id), FOREIGN KEY (productId) REFERENCES products(id))");
-
+// Call the function to initialize tables
+initializeTables();
+// CRUD operations for 'products' table
+app.post('/products', async (req, res) => {
+    const { title, price, description, category, image, rate, count } = req.body;
+    const result = await pool.query('INSERT INTO products (title, price, description, category, image, rate, count) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+        [title, price, description, category, image, rate, count]);
+    res.status(201).json(result.rows[0]);
 });
 
+app.get('/products', async (req, res) => {
+    const result = await pool.query('SELECT * FROM products');
+    res.status(200).json(result.rows);
+});
 
+app.put('/products/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, price, description, category, image, rate, count } = req.body;
+    const result = await pool.query(
+        'UPDATE products SET title = $1, price = $2, description = $3, category = $4, image = $5, rate = $6, count = $7 WHERE id = $8 RETURNING *',
+        [title, price, description, category, image, rate, count, id]);
+    res.status(200).json(result.rows[0]);
+});
 
-const PORT = 3002;
+app.delete('/products/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    await pool.query('DELETE FROM products WHERE id = $1', [id]);
+    res.status(200).send(`Product deleted with ID: ${id}`);
+});
+
+// Similar CRUD operations for 'users', 'cart', 'comments', and 'follows' tables
+// ...
+
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-app.get('/products', (req, res) => {
-    let query = "SELECT * FROM products";
-    const params = [];
-
-    if (req.query.search) {
-        query += " WHERE title LIKE ?";
-        console.log(query)
-        params.push(`%${req.query.search}%`);
-    }
-
-    
-    app.post('/users', (req, res) => {
-        const { username, password ,role} = req.body;
-        db.run("INSERT INTO user (username, password, isActive) VALUES (?, ?, false,role)", [username, password], function(err) {
-            if (err) {
-                res.status(400).send(err.message);
-                return;
-            }
-            res.status(201).send(`User created with ID: ${this.lastID}`);
-        });
-    });
-    
-    // READ all users
-    app.get('/users', (req, res) => {
-        db.all("SELECT * FROM user", [], (err, rows) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            res.status(200).json(rows);
-        });
-    });
-    
-    // READ a single user by ID
-    app.get('/users/:id', (req, res) => {
-        const id = req.params.id;
-        db.get("SELECT * FROM user WHERE id = ?", [id], (err, row) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            res.status(200).json(row);
-        });
-    });
-    
-    // UPDATE a user's isActive status
-    app.put('/users/:id/active', (req, res) => {
-        const id = req.params.id;
-        const { isActive } = req.body; // true or false
-        db.run("UPDATE user SET isActive = ? WHERE id = ?", [isActive, id], function(err) {
-            if (err) {
-                res.status(400).send(err.message);
-                return;
-            }
-            res.status(200).send(`User updated with ID: ${id}`);
-        });
-    });
-    
-    // DELETE a user
-    app.delete('/users/:id', (req, res) => {
-        const id = req.params.id;
-        db.run("DELETE FROM user WHERE id = ?", [id], function(err) {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            res.status(200).send(`User deleted with ID: ${id}`);
-        });
-    });
-    app.get('/current-user', (req, res) => {
-        db.get("SELECT username FROM user WHERE isActive = 1", (err, row) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            if (row) {
-                res.status(200).json({ username: row.username });
-            } else {
-                res.status(200).json({ username: null });
-            }
-        });
-    });
-    // CRUD operations for cart
-    
-    // CREATE a new cart entry
-    app.post('/cart', (req, res) => {
-        const { userId, productId } = req.body;
-        db.run("INSERT INTO cart (userId, productId) VALUES (?, ?)", [userId, productId], function(err) {
-            if (err) {
-                res.status(400).send(err.message);
-                return;
-            }
-            res.status(201).send(`Cart entry created with ID: ${this.lastID}`);
-        });
-    });
-    
-    // READ all cart entries
-    app.get('/cart', (req, res) => {
-        db.all("SELECT * FROM cart", [], (err, rows) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            res.status(200).json(rows);
-        });
-    });
-    
-    // READ a single cart entry by ID
-    app.get('/cart/:id', (req, res) => {
-        const id = req.params.id;
-        db.get("SELECT * FROM cart WHERE id = ?", [id], (err, row) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            res.status(200).json(row);
-        });
-    });
-    
-    // DELETE a cart entry
-    app.delete('/cart/:id', (req, res) => {
-        const id = req.params.id;
-        db.run("DELETE FROM cart WHERE id = ?", [id], function(err) {
-            if (err) {
-                res.status(500).send(err.message);
-                return;
-            }
-            res.status(200).send(`Cart entry deleted with ID: ${id}`);
-        });
-    });
-    
-    db.all(query, params, (err, rows) => {
-        if (err) {
-            res.status(500).send("Error in database operation");
-        } else {
-            res.json(rows);
-        }
-    });
-});
-
