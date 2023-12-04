b63b9fa54dcd46041ae10a0d659a4ca28c167f6f
shreyas bhaskar <bhaskar.s@northeastern.edu>
2023-12-04 14:36:25 -0500

    Added register

--- server.js
+++ server.js
@@ -152,6 +152,29 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.post('/register', async (req, res) => {
+    const { username, password, email, mobilenum } = req.body;
+    const isActive = true;  // Assuming new users are active by default
+    const role = 1;        // Default role
+
+    try {
+        // Check if username or email already exists
+        const checkUser = await pool.query('SELECT * FROM users WHERE username = $1 OR email = $2', [username, email]);
+        if (checkUser.rows.length > 0) {
+            return res.status(409).send('Username or email already exists');
+        }
+
+        // Insert new user
+        const newUser = await pool.query(
+            'INSERT INTO users (username, password, email, is_active, mobilenum, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            [username, password, email, isActive, mobilenum, role]
+        );
+        res.status(201).json(newUser.rows[0]);
+    } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 
 // Similar CRUD operations for 'users', 'cart', 'comments', and 'follows' tables
